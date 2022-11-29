df1833c34d47f3556567b0c4f719eb1abc8bae04
Sigurdur Thor <sigurdurtj@gmail.com>
2022-11-29 14:19:48 +0000

    added a operate function that takes an operator and 2 numbers then calls one of the functions above

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const add = function(add1, add2) {
-    const sum = add1 + add2;
+
+
+const add = function(num1, num2) {
+    const sum = num1 + num2;
     return sum;
 };
-console.log(add(2, 2));
 
-const subtract = function(sub1, sub2) {
-    const sum = sub1 - sub2; 
+const subtract = function(num1, num2) {
+    const sum = num1 - num2; 
     return sum;
 };
-console.log(subtract(2, 2));
 
-const multiply = function(mult1, mult2) {
-    let sum = mult1 * mult2;
+const multiply = function(num1, num2) {
+    let sum = num1 * num2;
     return sum; 
 };
-console.log(multiply(2, 2));
 
 const divide = function(num1, num2) {
     let sum =  num1 / num2;
     return sum;
 }
-console.log(divide(12, 2));
+
+function operate(num1, num2, operator) {
+switch (operator) {
+    case "+":
+        return add(num1, num2);
+    case "-":
+        return (num1, num2);
+    case "*":
+        return multiply(num1, num2);
+    case "/":
+        return divide(num1, num2);
+    }
+}
+console.log(operate(3, 3, "*"))
