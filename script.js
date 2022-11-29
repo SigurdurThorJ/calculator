75ab9fa8b766a6d329f87f3b237d7a4ef8b4940d
Sigurdur Thor <sigurdurtj@gmail.com>
2022-11-29 14:03:47 +0000

    added add, subtract, multiply, divide functions to JS

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function(add1, add2) {
+    const sum = add1 + add2;
+    return sum;
+};
+console.log(add(2, 2));
+
+const subtract = function(sub1, sub2) {
+    const sum = sub1 - sub2; 
+    return sum;
+};
+console.log(subtract(2, 2));
+
+const multiply = function(mult1, mult2) {
+    let sum = mult1 * mult2;
+    return sum; 
+};
+console.log(multiply(2, 2));
+
+const divide = function(num1, num2) {
+    let sum =  num1 / num2;
+    return sum;
+}
+console.log(divide(12, 2));
